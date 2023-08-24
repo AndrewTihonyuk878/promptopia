5a02a39b47699a69e945e4865e436b1b77d27f8f
Andrii <andrii.tykhoniuk@ccgames.io>
2023-08-24 10:59:57 +0200

    uncomented creator

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -30,7 +30,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image 
-            // src={post.creator.image}
+            src={post.creator.image}
             alt="user_image"
             width={40}
             height={40}
@@ -39,10 +39,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900"
             onClick={handleProfileClick}>
-              {/* {post.creator.username} */}
+              {post.creator.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {/* {post.creator.email} */}
+              {post.creator.email}
             </p>
           </div>
         </div>
@@ -66,7 +66,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {post.tag}
       </p>
 
-      {/* {session?.user.id === post.creator._id && pathName === '/profile' && (
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer"
@@ -82,7 +82,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             Delete
           </p>
         </div>
-      )} */}
+      )}
     </div>
   )
 }
